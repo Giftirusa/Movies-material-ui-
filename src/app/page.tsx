28998b77c6afd80584dec645447b4da2cf3642a6
Gift Irusa <137901838+Giftirusa@users.noreply.github.com>
2024-06-04 06:57:03 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,24 @@
-import { Box, Button, Card, CardActions, CardContent, Typography } from "@mui/material";
-import Image from "next/image";
+import { Box } from "@mui/material";
 import PrimarySearchAppBar from "./components/navbar";
+import MovieCard from "./components/movieCard";
 
 const bull = (
   <Box
     component="span"
     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
   >
-    
+
   </Box>
 );
 
 export default function Home() {
   return (
     <>
-    <PrimarySearchAppBar/>
-<Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+      <Box sx={{background:"black"}}>
+        <PrimarySearchAppBar />
+        <MovieCard genre="horror" rating={9} title={"The Outfit Full HD Movie Download"} img={"https://yts.homes/upload/31f15d5d781a7cc5f1b593b04f786c15.jpeg"} year={"2022"} />
+        <MovieCard genre="comedy" rating={5.5} title={"Movie2"} img={"https://yts.homes/upload/1257c12039728ee7ce62aeafc845e880.jpeg"} year={"2006"} />
+      </Box>
     </>
   );
 }
