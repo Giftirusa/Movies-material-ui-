0da88392764d5ddf9329d39b2e6a3655e9bc8a17
Gift Irusa <giftirusa@gmail.com>
2024-07-08 15:50:36 +0300

    connected to local backend

--- src/app/components/latestSection.tsx
+++ src/app/components/latestSection.tsx
@@ -1,8 +1,32 @@
 import { Box, Typography } from "@mui/material";
 import MovieCard from "./movieCard";
 import { Repeat } from "@mui/icons-material";
+import { useState,useEffect } from "react";
 
 export default function latestSection() {
+  let [movies,setMovies] = useState([])
+
+
+  async function newGetMovies() {
+    let resp = await fetch("http://localhost:8000/movies")
+    let data = resp.json()
+    console.log(data)
+
+    return data
+}
+
+async function setupMovies(){
+  let backendresp = await newGetMovies()
+
+  let listofMovies = backendresp
+  
+  setMovies(listofMovies)
+}
+
+useEffect(()=>{
+  setupMovies()
+},[])
+
     return (
         <Box sx={{
             display: "flex",
@@ -28,14 +52,10 @@ export default function latestSection() {
           gridTemplateRows:"repeat(2, 1fr)",
         }}>
 
-          <MovieCard genre="Documentary" rating={5.2} title={"ShadowRing"} img={"https://yts.homes/upload/106f0987f2bc4a61a7891f629c05c1bb.jpg"} year={"2013"} />
-          <MovieCard genre="Action" rating={5.7} title={"The Pink Cloud"} img={"https://yts.homes/upload/f4250f4cc267997666293fd666d5142d.jpg"} year={"2012"} />
-          <MovieCard genre="Action" rating={5.7} title={"Patriots Day"} img={"https://yts.homes/upload/09dedcc5eb27f43c346b6aac1b05cdba.jpg"} year={"2016"} />
-          <MovieCard genre="Action" rating={5.7} title={"The Big Doll House"} img={"https://yts.homes/upload/c347938c056f2843b82c96a4b793fddd.jpg"} year={"2022"} />
-          <MovieCard genre="Horror" rating={5.5} title={"Los Habitantes"} img={"https://yts.homes/upload/c229b0befa1703a6bdc09b3f3ee6b0c2.jpg"} year={"2023[SPANISH]"} />
-          <MovieCard genre="Action" rating={6.2} title={"Apple Music Live"} img={"https://yts.homes/upload/ee51566ff4fb552359c8a0c5e3718013.jpg"} year={"2024"} />
-          <MovieCard genre="Hindu" rating={4.5} title={"Punjab Nahi Jaungi"} img={"https://yts.homes/upload/7584271582d05aa28b3622181331e857.jpg"} year={"2017"} />
-          <MovieCard genre="Horror" rating={9} title={"Empire of Dreams"} img={"https://yts.homes/upload/956aa12c56c500556d28dda1a0632438.jpg"} year={"2004"} />
+{ movies.map((movie,idx) => {
+    return <MovieCard genre="Crime" rating={movie.rating} title={movie.title} img={movie.image} year={"2022"} />
+})}
+          
           </Box>
         </Box>
     )
